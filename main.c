3d229d67ec0265535a82dfc75e61e192f52b3783
WallaceJunior14 <wallacepfj1409@gmail.com>
2023-11-05 13:10:07 -0300

    Inimigo Concluido

--- main.c
+++ main.c
@@ -23,13 +23,30 @@
 #define QTD_ESPIAO2 4
 #define QTD_PORTA_AVIOES 2
 
-struct Peca_Principal
+struct Peca
 {
     int linha;
     int coluna; 
     char direcao;
     char id;
 };
+
+bool TIRO_ALEATORIO = true;
+struct Peca peca_temp_IA;
+int qtd_chances_IA;
+
+void limpa_tela()
+{
+    char comando[10];
+
+    #ifdef __linux__
+        strcpy(comando, "clear");
+        system(comando);
+    #elif _WIN32
+        strcpy(comando, "cls");
+        system(comando);
+    #endif
+}
 
 void inicializa_tabuleiro(char tabuleiro[][TAM])
 {
@@ -74,8 +91,11 @@ bool verifica_posicao(char tabuleiro[][TAM], int tamanho, int posicoes[][2])
     return true;
 }
 
-void cria_posicoes_pecas(char id, int posicoes[][2], int linha, int coluna, char direcao)
+void cria_posicoes_pecas(char id, int posicoes[][2], struct Peca peca)
 {
+    int linha = peca.linha, coluna = peca.coluna; 
+    char direcao = peca.direcao;
+
     switch (id)
     {
     case '0':
@@ -377,6 +397,26 @@ void cria_posicoes_pecas(char id, int posicoes[][2], int linha, int coluna, char
             posicoes[9][1] = coluna-4;
         }
         break;
+    
+    case 'T':
+        posicoes[0][0] = linha-1;
+        posicoes[0][1] = coluna;
+        posicoes[1][0] = linha+1;
+        posicoes[1][1] = coluna;
+        posicoes[2][0] = linha;
+        posicoes[2][1] = coluna-1;
+        posicoes[3][0] = linha;
+        posicoes[3][1] = coluna+1;
+
+        posicoes[4][0] = linha-1;
+        posicoes[4][1] = coluna+1;
+        posicoes[5][0] = linha-1;
+        posicoes[5][1] = coluna-1;
+        posicoes[6][0] = linha+1;
+        posicoes[6][1] = coluna+1;
+        posicoes[7][0] = linha+1;
+        posicoes[7][1] = coluna-1;
+        break;
     default:
         break;
     }
@@ -387,19 +427,45 @@ int informa_qtd_por_peca(char id)
     if(id == '0') return 1;
     if(id == '1') return 4;
     if(id == '2') return 4;
-    if(id == '3') return 5;
-    if(id == '4') return 5;
+    if(id == '3' || id == '4') return 6;
     if(id == '5') return 10;
     else return 0;
 }
 
-void salva_posicoes_iniciais(char *local_arquivo, int linha, int coluna, char orientacao, char id)
+void conta_pecas_tabuleiro(char tabuleiro[][TAM])
+{
+    int boia, aviao, submarino, espiao1, espiao2, p_avioes;
+    boia=aviao=submarino=espiao1=espiao2=p_avioes=0;
+
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+        {
+            if (tabuleiro[i][j] == '0') boia++;
+            if (tabuleiro[i][j] == '1') aviao++;
+            if (tabuleiro[i][j] == '2') submarino++;
+            if (tabuleiro[i][j] == '3') espiao1++;
+            if (tabuleiro[i][j] == '4') espiao2++;
+            if (tabuleiro[i][j] == '5') p_avioes++;
+        }
+    }
+
+    printf("\n--------------------------------------------\n");
+    printf("Boias: %d \n", boia);
+    printf("Aviao: %d \n", aviao/4);
+    printf("Submarino: %d \n", submarino/4);
+    printf("Espiao 1: %d \n", espiao1/6);
+    printf("Espiao 2: %d \n", espiao2/6);
+    printf("P. Avioes: %d \n", p_avioes/10);
+}
+
+void salva_posicoes_iniciais(char *local_arquivo, struct Peca peca)
 {
     FILE *arquivo = fopen(local_arquivo, "a+");
 
     if (arquivo != NULL)
     {
-        fprintf(arquivo, "%d %d %c %c\n", linha, coluna, orientacao, id);
+        fprintf(arquivo, "%d %d %c %c\n", peca.linha, peca.coluna, peca.direcao, peca.id);
     } else 
     {
         printf("Nao foi possivel abrir o arquivo: %s", local_arquivo);
@@ -408,7 +474,7 @@ void salva_posicoes_iniciais(char *local_arquivo, int linha, int coluna, char or
     fclose(arquivo);
 }
 
-void salva_posicoes_ataque(char *local_arquivo, struct Peca_Principal peca)
+bool salva_posicoes_ataque(char *local_arquivo, struct Peca peca)
 {
     FILE *arquivo = fopen(local_arquivo, "a+");
 
@@ -418,14 +484,16 @@ void salva_posicoes_ataque(char *local_arquivo, struct Peca_Principal peca)
     } else 
     {
         printf("Nao foi possivel abrir o arquivo: %s", local_arquivo);
+        return false;
     }
     
     fclose(arquivo);
+    return true;
 }
 
 bool carrega_posicoes_iniciais(char *local_arquivo, char tabuleiro[][TAM])
 {
-    struct Peca_Principal peca;
+    struct Peca peca;
 
     FILE *arquivo = fopen(local_arquivo, "r");
 
@@ -444,11 +512,36 @@ bool carrega_posicoes_iniciais(char *local_arquivo, char tabuleiro[][TAM])
 
         fflush(stdin);
 
-        cria_posicoes_pecas(peca.id, parte_pecas, peca.linha, peca.coluna, peca.direcao);
+        cria_posicoes_pecas(peca.id, parte_pecas, peca);
 
         for (int i = 0; i < qtd_por_peca; i++)
             tabuleiro[parte_pecas[i][0]][parte_pecas[i][1]] = peca.id;
         
+    }
+
+    fclose(arquivo);
+    return true;
+}
+
+bool carrega_posicoes_jogadas(char *local_arquivo, char tabuleiro[][TAM])
+{
+    struct Peca peca;
+
+    inicializa_tabuleiro(tabuleiro);
+
+    FILE *arquivo = fopen(local_arquivo, "r");
+
+    if (arquivo == NULL)
+    {
+        printf("Nao foi possivel abrir o arquivo: %s", local_arquivo);
+        return false;
+    }
+
+    while (fscanf(arquivo, "%d%d%s", &peca.linha, &peca.coluna, &peca.id) != EOF)
+    {
+        fflush(stdin);
+
+        tabuleiro[peca.linha][peca.coluna] = peca.id;
     }
 
     fclose(arquivo);
@@ -465,9 +558,9 @@ char gera_direcao_randomico()
     return direcao[posicao];
 }
 
-struct Peca_Principal solicita_posicao_direaco(bool pega_direcao)
+struct Peca solicita_posicao_direaco(bool pega_direcao)
 {
-    struct Peca_Principal peca; 
+    struct Peca peca; 
     char confirmacao;
 
     printf("\n-------------------------------------------\n");
@@ -500,7 +593,7 @@ void posiciona_peca(char tabuleiro[][TAM], char id, int quantidade, int qtd_posi
     srand(time(NULL));
 
     int contador = 0, parte_pecas[qtd_posicoes_peca][2];
-    struct Peca_Principal peca;
+    struct Peca peca;
 
     if (posicionamento_automatico)
     {
@@ -509,15 +602,16 @@ void posiciona_peca(char tabuleiro[][TAM], char id, int quantidade, int qtd_posi
             peca.linha = rand() % 19;
             peca.coluna = rand() % 19;
             peca.direcao = id=='0' ? '-': gera_direcao_randomico();
+            peca.id = id;
 
-            cria_posicoes_pecas(id, parte_pecas, peca.linha, peca.coluna, peca.direcao);
+            cria_posicoes_pecas(id, parte_pecas, peca);
 
             if (verifica_posicao(tabuleiro, qtd_posicoes_peca, parte_pecas))
             {
                 for (int i = 0; i < qtd_posicoes_peca; i++)
                     tabuleiro[parte_pecas[i][0]][parte_pecas[i][1]] = id;
 
-                salva_posicoes_iniciais(local_arquivo, peca.linha, peca.coluna, peca.direcao, id);
+                salva_posicoes_iniciais(local_arquivo, peca);
 
             } else continue;
 
@@ -533,15 +627,16 @@ void posiciona_peca(char tabuleiro[][TAM], char id, int quantidade, int qtd_posi
             mostra_tabuleiro(tabuleiro);
 
             peca = solicita_posicao_direaco(true);
+            peca.id = id;
 
-            cria_posicoes_pecas(id, parte_pecas, peca.linha, peca.coluna, peca.direcao);
+            cria_posicoes_pecas(id, parte_pecas, peca);
 
             if (verifica_posicao(tabuleiro, qtd_posicoes_peca, parte_pecas))
             {
                 for (int i = 0; i < qtd_posicoes_peca; i++)
                     tabuleiro[parte_pecas[i][0]][parte_pecas[i][1]] = id;
 
-                salva_posicoes_iniciais(local_arquivo, peca.linha, peca.coluna, peca.direcao, id);
+                salva_posicoes_iniciais(local_arquivo, peca);
 
             } else continue;
 
@@ -562,26 +657,28 @@ void inicializa_player(char tabuleiro[][TAM], bool posicionamento_automatico, ch
     posiciona_peca(tabuleiro, ID_PORTA_AVIOES, QTD_PORTA_AVIOES, 10, posicionamento_automatico, local_salvamento);
 }
 
-char verifica_ataque(char tabuleiro[][TAM], struct Peca_Principal peca)
+char verifica_ataque(bool jogador, char tabuleiro_real[][TAM], char tabuleiro_sombra[][TAM], struct Peca peca)
 {
-    if (tabuleiro[peca.linha][peca.coluna] == MAR)
+    if (tabuleiro_sombra[peca.linha][peca.coluna] != MAR )
     {
-        return TIRO_NA_AGUA;
+        if (jogador) printf("\nPosicao ja jogada. Informe outra posicao!\n");
+        return ' ';
     } else 
     {
-        return tabuleiro[peca.linha][peca.coluna];
+        return tabuleiro_real[peca.linha][peca.coluna] == MAR ? TIRO_NA_AGUA : tabuleiro_real[peca.linha][peca.coluna];
     }
 }
 
-void ataque(bool jogador, char tabuleiro_real[][TAM], char tabuleiro_sombra[][TAM])
+bool ataque(bool jogador, char tabuleiro_real[][TAM], char tabuleiro_sombra[][TAM])
 {
-    struct Peca_Principal peca;
+    int posicoes_ataque_IA[7][2], posicoes_temp[7], cont = 0, posicao;
+    struct Peca peca;
 
     srand(time(NULL));
 
     if (jogador)
     {
-        printf("\n================ VEZ DO JOGADOR ================\n");
+        // ALGORITIMO DE ATAQUE DO JOGADOR
 
         mostra_tabuleiro(tabuleiro_sombra);
 
@@ -590,26 +687,97 @@ void ataque(bool jogador, char tabuleiro_real[][TAM], char tabuleiro_sombra[][TA
         scanf("%d", &peca.linha);
         printf("\n-> Coluna: ");
         scanf("%d", &peca.coluna);
-        printf("\n--------------------");
+        printf("\n--------------------\n");
 
-        peca.id = verifica_ataque(tabuleiro_real, peca);
+        peca.id = verifica_ataque(true, tabuleiro_real, tabuleiro_sombra, peca);
+
+        if (peca.id == ' ')
+        {
+            return ataque(true, tabuleiro_real, tabuleiro_sombra);
+        }
+
         tabuleiro_sombra[peca.linha][peca.coluna] = peca.id;
 
         salva_posicoes_ataque("./ATAQUES_JOGADOR.TXT", peca);
+
+        mostra_tabuleiro(tabuleiro_sombra);
     } else
     {
-        printf("\n================ VEZ DA IA ================\n");
-        peca.linha = rand() % 19;
-        peca.coluna = rand() % 19;
-        peca.id = verifica_ataque(tabuleiro_real, peca);
-
-        tabuleiro_sombra[peca.linha][peca.coluna] = peca.id;
-
-        salva_posicoes_ataque("./ATAQUES_INIMIGO.TXT", peca);
+        // ALGORITIMO DE ATAQUE DA IA
         
-        mostra_tabuleiro(tabuleiro_sombra);
+        if(TIRO_ALEATORIO) 
+        {
+            peca.linha = rand() % 19;
+            peca.coluna = rand() % 19;
+            peca.id = verifica_ataque(false, tabuleiro_real, tabuleiro_sombra, peca);
+
+            if (peca.id == ' ') {
+                return ataque(false, tabuleiro_real, tabuleiro_sombra);
+            } else if (peca.id != '#' && peca.id != '0')
+            {
+                peca_temp_IA = peca;
+                TIRO_ALEATORIO = false;
+                qtd_chances_IA = 7;
+            }
+
+            tabuleiro_sombra[peca.linha][peca.coluna] = peca.id;
+
+            salva_posicoes_ataque("./ATAQUES_INIMIGO.TXT", peca);
+            
+            mostra_tabuleiro(tabuleiro_sombra);
+        } else 
+        {
+            if (qtd_chances_IA <=0)
+            {
+                TIRO_ALEATORIO = true;
+                qtd_chances_IA = 7;
+                return ataque(false, tabuleiro_real, tabuleiro_sombra);
+            } else
+            {
+                cria_posicoes_pecas('T', posicoes_ataque_IA, peca_temp_IA );
+
+                do
+                {
+                    posicao = rand() % 7;
+
+                    for (int i = 0; i < 7; i++)
+                    {
+                        if (posicoes_temp[i] == posicao) posicao = rand() % 7;
+                        else 
+                        {
+                            posicoes_temp[cont] = posicao;
+                            break;
+                        }
+                    }    
+                    
+                    peca.linha = posicoes_ataque_IA[posicao][0];
+                    peca.coluna = posicoes_ataque_IA[posicao][1];
+
+                    printf("l: %d, c: %d\n", peca.linha, peca.coluna);
+                    
+                    peca.id = verifica_ataque(false, tabuleiro_real, tabuleiro_sombra, peca);
+                    printf("id: %c\n", peca.id);
+                    cont++;
+                } while (peca.id == ' ');
+                
+
+                if (peca.id != '#')
+                {
+                    peca_temp_IA = peca;
+                    TIRO_ALEATORIO = false;
+                    qtd_chances_IA = 7;
+                }
+
+                tabuleiro_sombra[peca.linha][peca.coluna] = peca.id;
+
+                salva_posicoes_ataque("./ATAQUES_INIMIGO.TXT", peca);
+                
+                
+                mostra_tabuleiro(tabuleiro_sombra);
+            }
+        }
+
     }
-    
 }
 
 int verifica_pontuacao(char tabuleiro[][TAM])
@@ -656,12 +824,14 @@ void menu_batalha(char tabuleiro_IA[][TAM],char tab_sombra_IA[][TAM], char tabul
     printf("||               MENU DE GUERRA             ||\n");
     printf("==============================================\n\n");
 
+
     if (pontos_IA == 30) anuncia_vencedor(false);
     else if(pontos_jogador == 30) anuncia_vencedor(true);
     else{
         printf("Pontos Jogador: %d\n", pontos_jogador);
         printf("Pontos IA: %d\n", pontos_IA);
     }
+
 
     printf("-- MENU --\n");
     printf("1) Atacar\n2) Sair\n");
@@ -670,7 +840,9 @@ void menu_batalha(char tabuleiro_IA[][TAM],char tab_sombra_IA[][TAM], char tabul
     switch (opcoes_menu_guerra)
     {
     case 1:
+        printf("\n================ VEZ DO JOGADOR ================\n");
         ataque(true, tabuleiro_IA, tab_sombra_IA); // vez do jogador
+        printf("\n================ VEZ DA IA ================\n");
         ataque(false, tabuleiro_jogador, tab_sombra_jogador); // vez da IA
 
         menu_batalha(tabuleiro_IA, tab_sombra_IA, tabuleiro_jogador, tab_sombra_jogador);
@@ -692,8 +864,7 @@ void menu_inicial(char tabuleiro_IA[][TAM], char tabuleiro_jogador[][TAM])
     int opcao_menu_inicial;
     char tab_sombra_IA[TAM][TAM], tab_sombra_jogador[TAM][TAM];
 
-    inicializa_tabuleiro(tab_sombra_IA);
-    inicializa_tabuleiro(tab_sombra_jogador);
+    limpa_tela();
 
     printf("\n============================================\n");
     printf("||              BATALHA NAVAL             ||\n");
@@ -707,9 +878,7 @@ void menu_inicial(char tabuleiro_IA[][TAM], char tabuleiro_jogador[][TAM])
     {
     case 1:
         inicializa_player(tabuleiro_IA, true, "./MATRIZ_INIMIGO.TXT");
-        inicializa_player(tabuleiro_jogador, false, "./MATRIZ_JOGADOR.TXT");
-
-        //mostra_tabuleiro(tabuleiro_jogador);
+        inicializa_player(tabuleiro_jogador, true, "./MATRIZ_JOGADOR.TXT");
 
         menu_batalha(tabuleiro_IA, tab_sombra_IA, tabuleiro_jogador, tab_sombra_jogador);
         break;
@@ -717,7 +886,13 @@ void menu_inicial(char tabuleiro_IA[][TAM], char tabuleiro_jogador[][TAM])
         carrega_posicoes_iniciais("./MATRIZ_INIMIGO.TXT", tabuleiro_IA);
         carrega_posicoes_iniciais("./MATRIZ_JOGADOR.TXT", tabuleiro_jogador);
 
+        carrega_posicoes_jogadas("./ATAQUES_INIMIGO.TXT", tab_sombra_jogador);
+        carrega_posicoes_jogadas("./ATAQUES_JOGADOR.TXT", tab_sombra_IA);
+
         menu_batalha(tabuleiro_IA, tab_sombra_IA, tabuleiro_jogador, tab_sombra_jogador);
+        break;
+    case 3:
+
         break;
     default:
         printf("__Mensagem__\n");
